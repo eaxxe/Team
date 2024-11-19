c26649899488eb8bbb6e8d9ae889286354a29d51
Eaxxe <eaxxeg@gmail.com>
2024-11-19 12:37:58 +0300

    added: Implemented the Magazine class
    * `private string? _name`
    * `private DateTime _dateOfPublicationMagazine`
    * `private int _magazineCirculation`
    
    * `public string Name { get; set; }`
    * `public DateTime DateOfPublicationMagazine { get; set; }`
    * `public int MagazineCirculation { get; set; }`
    
    * `public Magazine(string name, DateTime dateOfPublicationMagazine, int magazineCirculation)`
    * `public Magazine()`
    
    * `public override string ToString()`

---
+++ Magazine.cs
@@ -0,0 +1,63 @@
+﻿namespace Team
+{
+    public class Magazine
+    {
+        private string? _name;
+        private DateTime _dateOfPublicationMagazine;
+        private int _magazineCirculation;
+        //Article[] articles;
+        //Frequency frequency;
+
+        public string Name
+        {
+            set
+            {
+                if (!string.IsNullOrEmpty(_name)) _name = value;
+                else throw new ArgumentException("The provided name is either empty or uninitialized.", nameof(value));
+            }
+            get => _name ?? string.Empty;
+        }
+
+        public DateTime DateOfPublicationMagazine
+        {
+            set
+            {
+                if (value != default) _dateOfPublicationMagazine = value;
+                else throw new ArgumentException("The provided date of publication is invalid.", nameof(value));
+            }
+            get => _dateOfPublicationMagazine;
+        }
+
+        public int MagazineCirculation
+        {
+            set
+            {
+                if (value > 0) _magazineCirculation = value;
+                else throw new ArgumentException("The provided circulation of magazine cannot be a negative or equals zero", nameof(value));
+            }
+            get => _magazineCirculation;
+        }
+
+        public Magazine(string name, DateTime dateOfPublicationMagazine, int magazineCirculation)
+        {
+            Name = name;
+            DateOfPublicationMagazine = dateOfPublicationMagazine;
+            MagazineCirculation = magazineCirculation;
+        }
+
+        public Magazine()
+        {
+            _name = "Model";
+            _dateOfPublicationMagazine = DateTime.Today;
+            _magazineCirculation = 10000;
+        }
+
+        public override string ToString()
+        {
+            return $"Name of magazine: {_name}\n" +
+                $"Date of publication: {_dateOfPublicationMagazine}\n" +
+                $"Magazine circulation: {_magazineCirculation}\n" +
+                $"List of articles : \n";
+        }
+    }
+}
